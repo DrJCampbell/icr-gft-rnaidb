cc73cdbcd22039fe35f8dc27bc4ae10a8a6a0703
Wenbin Wei <Wenbin.Wei@icr.ac.uk>
2014-10-30 10:56:59 +0000

    This script should be run after RnaiAnalysis_ver_1.251.sql and before adding RNAi screen data.

---
+++ RNAiDB_initial_inserts.sql
@@ -0,0 +1,162 @@
+insert into Instrument_used (Instrument_name) values ('1S10');
+insert into Instrument_used (Instrument_name) values ('1C11');
+
+insert into Name_of_set_if_isogenic (Name_of_set_if_isogenic) values ('ARID1A-set');
+insert into Name_of_set_if_isogenic (Name_of_set_if_isogenic) values ('KRAS-set');
+insert into Name_of_set_if_isogenic (Name_of_set_if_isogenic) values ('PTEN-set');
+insert into Name_of_set_if_isogenic (Name_of_set_if_isogenic) values ('NA');
+
+
+
+insert into Tissue_type (Tissue_of_origin) values ('ADRENAL_GLAND');
+insert into Tissue_type (Tissue_of_origin) values ('AUTONOMIC_GANGLIA');
+insert into Tissue_type (Tissue_of_origin) values ('BILIARY_TRACT');
+insert into Tissue_type (Tissue_of_origin) values ('BONE');
+insert into Tissue_type (Tissue_of_origin) values ('BREAST');
+insert into Tissue_type (Tissue_of_origin) values ('CENTRAL_NERVOUS_SYSTEM');
+insert into Tissue_type (Tissue_of_origin) values ('CERVICAL');
+insert into Tissue_type (Tissue_of_origin) values ('ENDOMETRIUM');
+insert into Tissue_type (Tissue_of_origin) values ('HAEMATOPOIETIC_AND_LYMPHOID_TISSUE');
+insert into Tissue_type (Tissue_of_origin) values ('HEADNECK');
+insert into Tissue_type (Tissue_of_origin) values ('KIDNEY');
+insert into Tissue_type (Tissue_of_origin) values ('LARGE_INTESTINE');
+insert into Tissue_type (Tissue_of_origin) values ('LIVER');
+insert into Tissue_type (Tissue_of_origin) values ('LUNG');
+insert into Tissue_type (Tissue_of_origin) values ('LYMPHOBLASTOMA');
+insert into Tissue_type (Tissue_of_origin) values ('NA');
+insert into Tissue_type (Tissue_of_origin) values ('OESOPHAGUS');
+insert into Tissue_type (Tissue_of_origin) values ('OVARY');
+insert into Tissue_type (Tissue_of_origin) values ('PANCREAS');
+insert into Tissue_type (Tissue_of_origin) values ('PLACENTA');
+insert into Tissue_type (Tissue_of_origin) values ('PLEURA');
+insert into Tissue_type (Tissue_of_origin) values ('PROSTATE');
+insert into Tissue_type (Tissue_of_origin) values ('SALIVARY_GLAND');
+insert into Tissue_type (Tissue_of_origin) values ('SKIN');
+insert into Tissue_type (Tissue_of_origin) values ('SMALL_INTESTINE');
+insert into Tissue_type (Tissue_of_origin) values ('SOFT_TISSUE');
+insert into Tissue_type (Tissue_of_origin) values ('STOMACH');
+insert into Tissue_type (Tissue_of_origin) values ('TESTIS');
+insert into Tissue_type (Tissue_of_origin) values ('THYROID');
+insert into Tissue_type (Tissue_of_origin) values ('UPPER_AERODIGESTIVE_TRACT');
+insert into Tissue_type (Tissue_of_origin) values ('URINARY_TRACT');
+insert into Tissue_type (Tissue_of_origin) values ('VULVA');
+
+
+insert into Transfection_reagent_used (Transfection_reagent) values ('Lipofectamine 2000');
+insert into Transfection_reagent_used (Transfection_reagent) values ('Lipofectamine 3000');
+insert into Transfection_reagent_used (Transfection_reagent) values ('Lipofectamine RNAi max');
+insert into Transfection_reagent_used (Transfection_reagent) values ('Dharmafect 3');
+insert into Transfection_reagent_used (Transfection_reagent) values ('Dharmafect 4');
+insert into Transfection_reagent_used (Transfection_reagent) values ('Oligofectamine');
+insert into Transfection_reagent_used (Transfection_reagent) values ('Effectene');
+
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/Kinome_96_plateconf.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/Kinome_96_validation_plateconf.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/KS_TS_384_plateconf1.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/KS_TS_384_plateconf2.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/KS_TS_384_plateconf3.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/KS_TS_384_plateconf4.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/KS_TS_DR_384_plateconf1.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/KS_TS_DR_384_plateconf2.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/KS_TS_DR_384_plateconf3.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/KS_TS_DR_384_plateconf4.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/KS_TS_DR_MT_PH_384_plateconf1.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/KS_TS_DR_MT_PH_384_plateconf2.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/KS_TS_DR_MT_PH_384_plateconf3.txt');
+insert into Plateconf_file_path(Plateconf_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_conf_folder/plateconf_KS_TS_CGC.txt');
+
+insert into Platelist_file_path(Platelist_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_list_folder/Kinome_96_validation_platelist.txt');
+insert into Platelist_file_path(Platelist_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_list_folder/platelist_96_2replicates.txt');
+insert into Platelist_file_path(Platelist_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_list_folder/platelist_96.txt');
+insert into Platelist_file_path(Platelist_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_list_folder/platelist_384.txt');
+insert into Platelist_file_path(Platelist_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_list_folder/platelist_KS_TS_CGC.txt');
+insert into Platelist_file_path(Platelist_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_list_folder/platelist_p3r3.txt');
+insert into Platelist_file_path(Platelist_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_list_folder/platelist_p7r3.txt');
+insert into Platelist_file_path(Platelist_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_list_folder/platelist_p7r3_TKCC07.txt');
+insert into Platelist_file_path(Platelist_file_location) values ('/Users/wwei/icr-gft-rnaidb/plate_list_folder/platelist_p7r3c1.txt');
+
+insert into Template_library (Template_library_name) values ('Epigenetics_template');
+insert into Template_library (Template_library_name) values ('Kinome_96_template');
+insert into Template_library (Template_library_name) values ('Kinome_96_validation_template');
+insert into Template_library (Template_library_name) values ('KS_TS_384_template');
+insert into Template_library (Template_library_name) values ('KS_TS_DR_384_template');
+insert into Template_library (Template_library_name) values ('KS_TS_DR_MT_PH_384_template1');
+insert into Template_library (Template_library_name) values ('KS_TS_DR_MT_PH_384_template2');
+insert into Template_library (Template_library_name) values ('KS_TS_DR_MT_PH_384_template3');
+insert into Template_library (Template_library_name) values ('KS_TS_CGC_384_template');
+insert into Template_library (Template_library_name) values ('KS_TS_CGC_WNT_384_template');
+select * from Template_library;
+UPDATE Template_library SET Template_library_name = 'KS_TS_CGC_384_template' WHERE Template_library_name = 'KS_TS_DR_CGC_384_template';
+
+insert into Template_library_file_path (Template_library_file_location) values ('/Users/wwei/icr-gft-rnaidb/template_library_folder/Epigenetics_template.txt');
+insert into Template_library_file_path (Template_library_file_location) values ('/Users/wwei/icr-gft-rnaidb/template_library_folder/Kinome_96_template.txt');
+insert into Template_library_file_path (Template_library_file_location) values ('/Users/wwei/icr-gft-rnaidb/template_library_folder/Kinome_96_validation_template.txt');
+insert into Template_library_file_path (Template_library_file_location) values ('/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_384_template.txt');
+insert into Template_library_file_path (Template_library_file_location) values ('/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_DR_384_template.txt');
+insert into Template_library_file_path (Template_library_file_location) values ('/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_DR_MT_PH_384_template1.txt');
+insert into Template_library_file_path (Template_library_file_location) values ('/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_DR_MT_PH_384_template2.txt');
+insert into Template_library_file_path (Template_library_file_location) values ('/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_DR_MT_PH_384_template3.txt');
+insert into Template_library_file_path (Template_library_file_location) values ('/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_CGC_384_template.txt');
+insert into Template_library_file_path (Template_library_file_location) values ('/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_CGC_WNT_384_template.txt');
+select * from Template_library_file_path;
+UPDATE Template_library_file_path SET Template_library_file_location = '/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_CGC_384_template.txt' WHERE Template_library_file_location = '/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_DR_CGC_384_template.txt';
+
+BEGIN;
+LOAD DATA LOCAL INFILE '/Users/wwei/icr-gft-rnaidb/template_library_folder/Epigenetics_template.txt' INTO TABLE Template_library_file FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\n" IGNORE 1 LINES (@vPlate_templib, @vWell_templib, @vGene_symbol_templib, @vEntrez_gene_id_templib, @vSub_lib) SET Plate_templib = nullif(@vPlate_templib, ''), Well_templib = nullif(@vWell_templib,''), Gene_symbol_templib = nullif(@vGene_symbol_templib,''), Entrez_gene_id_templib = nullif(@vEntrez_gene_id_templib,''), Sub_lib = nullif(@vSub_lib,'');
+UPDATE Template_library_file SET Template_library_Template_library_ID = (select Template_library_ID from Template_library where Template_library_name='Epigenetics_template') WHERE Template_library_Template_library_ID IS NULL;
+select * from Template_library_file where Well_templib='A03';
+COMMIT;
+ 
+BEGIN;
+LOAD DATA LOCAL INFILE '/Users/wwei/icr-gft-rnaidb/template_library_folder/Kinome_96_template.txt' INTO TABLE Template_library_file FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\n" IGNORE 1 LINES (@vPlate_templib, @vWell_templib, @vGene_symbol_templib, @vEntrez_gene_id_templib, @vSub_lib) SET Plate_templib = nullif(@vPlate_templib, ''), Well_templib = nullif(@vWell_templib,''), Gene_symbol_templib = nullif(@vGene_symbol_templib,''), Entrez_gene_id_templib = nullif(@vEntrez_gene_id_templib,''), Sub_lib = nullif(@vSub_lib,'');
+UPDATE Template_library_file SET Template_library_Template_library_ID = (select Template_library_ID from Template_library where Template_library_name='Kinome_96_template') WHERE Template_library_Template_library_ID IS NULL;
+select * from Template_library_file where Well_templib='A03';
+COMMIT;
+
+BEGIN;
+LOAD DATA LOCAL INFILE '/Users/wwei/icr-gft-rnaidb/template_library_folder/Kinome_96_validation_template.txt' INTO TABLE Template_library_file FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\n" IGNORE 1 LINES (@vPlate_templib, @vWell_templib, @vGene_symbol_templib, @vEntrez_gene_id_templib, @vSub_lib) SET Plate_templib = nullif(@vPlate_templib, ''), Well_templib = nullif(@vWell_templib,''), Gene_symbol_templib = nullif(@vGene_symbol_templib,''), Entrez_gene_id_templib = nullif(@vEntrez_gene_id_templib,''), Sub_lib = nullif(@vSub_lib,'');
+UPDATE Template_library_file SET Template_library_Template_library_ID = (select Template_library_ID from Template_library where Template_library_name='Kinome_96_validation_template') WHERE Template_library_Template_library_ID IS NULL;
+select * from Template_library_file where Well_templib='A03';
+COMMIT;
+
+BEGIN;
+LOAD DATA LOCAL INFILE '/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_384_template.txt' INTO TABLE Template_library_file FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\n" IGNORE 1 LINES (@vPlate_templib, @vWell_templib, @vGene_symbol_templib, @vEntrez_gene_id_templib, @vSub_lib) SET Plate_templib = nullif(@vPlate_templib, ''), Well_templib = nullif(@vWell_templib,''), Gene_symbol_templib = nullif(@vGene_symbol_templib,''), Entrez_gene_id_templib = nullif(@vEntrez_gene_id_templib,''), Sub_lib = nullif(@vSub_lib,'');
+UPDATE Template_library_file SET Template_library_Template_library_ID = (select Template_library_ID from Template_library where Template_library_name='KS_TS_384_template') WHERE Template_library_Template_library_ID IS NULL;
+select * from Template_library_file where Well_templib='A03';
+COMMIT;
+
+BEGIN;
+LOAD DATA LOCAL INFILE '/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_DR_384_template.txt' INTO TABLE Template_library_file FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\n" IGNORE 1 LINES (@vPlate_templib, @vWell_templib, @vGene_symbol_templib, @vEntrez_gene_id_templib, @vSub_lib) SET Plate_templib = nullif(@vPlate_templib, ''), Well_templib = nullif(@vWell_templib,''), Gene_symbol_templib = nullif(@vGene_symbol_templib,''), Entrez_gene_id_templib = nullif(@vEntrez_gene_id_templib,''), Sub_lib = nullif(@vSub_lib,'');
+UPDATE Template_library_file SET Template_library_Template_library_ID = (select Template_library_ID from Template_library where Template_library_name='KS_TS_DR_384_template') WHERE Template_library_Template_library_ID IS NULL;
+select * from Template_library_file where Well_templib='A03';
+COMMIT;
+
+BEGIN;
+LOAD DATA LOCAL INFILE '/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_DR_MT_PH_384_template1.txt' INTO TABLE Template_library_file FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\n" IGNORE 1 LINES (@vPlate_templib, @vWell_templib, @vGene_symbol_templib, @vEntrez_gene_id_templib, @vSub_lib) SET Plate_templib = nullif(@vPlate_templib, ''), Well_templib = nullif(@vWell_templib,''), Gene_symbol_templib = nullif(@vGene_symbol_templib,''), Entrez_gene_id_templib = nullif(@vEntrez_gene_id_templib,''), Sub_lib = nullif(@vSub_lib,'');
+UPDATE Template_library_file SET Template_library_Template_library_ID = (select Template_library_ID from Template_library where Template_library_name='KS_TS_DR_MT_PH_384_template1') WHERE Template_library_Template_library_ID IS NULL;
+select * from Template_library_file where Well_templib='A03';
+COMMIT;
+
+BEGIN;
+LOAD DATA LOCAL INFILE '/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_DR_MT_PH_384_template2.txt' INTO TABLE Template_library_file FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\n" IGNORE 1 LINES (@vPlate_templib, @vWell_templib, @vGene_symbol_templib, @vEntrez_gene_id_templib, @vSub_lib) SET Plate_templib = nullif(@vPlate_templib, ''), Well_templib = nullif(@vWell_templib,''), Gene_symbol_templib = nullif(@vGene_symbol_templib,''), Entrez_gene_id_templib = nullif(@vEntrez_gene_id_templib,''), Sub_lib = nullif(@vSub_lib,'');
+UPDATE Template_library_file SET Template_library_Template_library_ID = (select Template_library_ID from Template_library where Template_library_name='KS_TS_DR_MT_PH_384_template2') WHERE Template_library_Template_library_ID IS NULL;
+select * from Template_library_file where Well_templib='A03';
+COMMIT;
+
+BEGIN;
+LOAD DATA LOCAL INFILE '/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_DR_MT_PH_384_template3.txt' INTO TABLE Template_library_file FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\n" IGNORE 1 LINES (@vPlate_templib, @vWell_templib, @vGene_symbol_templib, @vEntrez_gene_id_templib, @vSub_lib) SET Plate_templib = nullif(@vPlate_templib, ''), Well_templib = nullif(@vWell_templib,''), Gene_symbol_templib = nullif(@vGene_symbol_templib,''), Entrez_gene_id_templib = nullif(@vEntrez_gene_id_templib,''), Sub_lib = nullif(@vSub_lib,'');
+UPDATE Template_library_file SET Template_library_Template_library_ID = (select Template_library_ID from Template_library where Template_library_name='KS_TS_DR_MT_PH_384_template3') WHERE Template_library_Template_library_ID IS NULL;
+select * from Template_library_file where Well_templib='A03';
+COMMIT;
+
+BEGIN;
+LOAD DATA LOCAL INFILE '/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_CGC_384_template.txt' INTO TABLE Template_library_file FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\n" IGNORE 1 LINES (@vPlate_templib, @vWell_templib, @vGene_symbol_templib, @vEntrez_gene_id_templib, @vSub_lib) SET Plate_templib = nullif(@vPlate_templib, ''), Well_templib = nullif(@vWell_templib,''), Gene_symbol_templib = nullif(@vGene_symbol_templib,''), Entrez_gene_id_templib = nullif(@vEntrez_gene_id_templib,''), Sub_lib = nullif(@vSub_lib,'');
+UPDATE Template_library_file SET Template_library_Template_library_ID = (select Template_library_ID from Template_library where Template_library_name='KS_TS_CGC_384_template') WHERE Template_library_Template_library_ID IS NULL;
+select * from Template_library_file where Well_templib='A03';
+COMMIT;
+
+BEGIN;
+LOAD DATA LOCAL INFILE '/Users/wwei/icr-gft-rnaidb/template_library_folder/KS_TS_CGC_WNT_384_template.txt' INTO TABLE Template_library_file FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\n" IGNORE 1 LINES (@vPlate_templib, @vWell_templib, @vGene_symbol_templib, @vEntrez_gene_id_templib, @vSub_lib) SET Plate_templib = nullif(@vPlate_templib, ''), Well_templib = nullif(@vWell_templib,''), Gene_symbol_templib = nullif(@vGene_symbol_templib,''), Entrez_gene_id_templib = nullif(@vEntrez_gene_id_templib,''), Sub_lib = nullif(@vSub_lib,'');
+UPDATE Template_library_file SET Template_library_Template_library_ID = (select Template_library_ID from Template_library where Template_library_name='KS_TS_CGC_WNT_384_template') WHERE Template_library_Template_library_ID IS NULL;
+select * from Template_library_file where Well_templib='A03';
+COMMIT;
